c57cb89f42503d2d6c590cc721ffee5132f98d8f
dplocki <dawid.plocki@gmail.com>
2024-05-22 21:22:13 +0200

    Clean up of the App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: `
+    <h1>{{ title }}</h1>
+  `,
 })
 export class AppComponent {
-  title = 'datamancer';
+  title = 'Datamancer';
 }
