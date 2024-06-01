cfeadc82d6d4436a02c63844db3c87e9056f446c
dplocki <dawid.plocki@gmail.com>
2024-06-01 17:10:24 +0200

    A step closer to understand the TreeView

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -8,67 +8,36 @@ import { MatTreeModule } from '@angular/material/tree';
 import { BehaviorSubject, Observable, map, merge, of } from 'rxjs';
 import { DatabaseManagerService } from '../services/database-manager.service';
 
-/** Flat node with expandable and level information */
 export class DynamicFlatNode {
   constructor(
     public item: string,
     public level = 1,
-    public expandable = false,
-    public isLoading = false,
+    public isExpandable = false,
   ) { }
 }
 
-/**
- * Database for dynamic data. When expanding a node in the tree, the data source will need to fetch
- * the descendants data from the database.
- */
-@Injectable({ providedIn: 'root' })
-export class DynamicDatabase {
-  dataMap = new Map<string, string[]>([
-    ['Fruits', ['Apple', 'Orange', 'Banana']],
-    ['Vegetables', ['Tomato', 'Potato', 'Onion']],
-    ['Apple', ['Fuji', 'Macintosh']],
-    ['Onion', ['Yellow', 'White', 'Purple']],
-  ]);
-
-  rootLevelNodes: string[] = ['Fruits', 'Vegetables'];
-
-  /** Initial data from database */
-  initialData(): DynamicFlatNode[] {
-    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
-  }
-
-  getChildren(node: string): string[] | undefined {
-    return this.dataMap.get(node);
-  }
-
-  isExpandable(node: string): boolean {
-    return this.dataMap.has(node);
-  }
-}
-
-/**
- * File database, it can build a tree structured Json object from string.
- * Each node in Json object represents a file or a directory. For a file, it has filename and type.
- * For a directory, it has filename and children (a list of files or directories).
- * The input will be a json object string, and the output is a list of `FileNode` with nested
- * structure.
- */
 export class DynamicDataSource implements DataSource<DynamicFlatNode> {
+  private dataSubject = new BehaviorSubject<DynamicFlatNode[]>([]);
 
-  constructor(
-    private _treeControl: FlatTreeControl<DynamicFlatNode>,
-  ) { }
+  constructor(private database: DatabaseManagerService) {}
 
   connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
-    console.log('collectionViewer', collectionViewer);
-    return of([
-    ]);
+    collectionViewer.viewChange.subscribe(p => console.log('a', p));
+
+    return of(this.database.getTablesList().map((s: string) => (new DynamicFlatNode(s, 0, true))));
   }
 
-  disconnect(collectionViewer: CollectionViewer): void { }
-}
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.dataSubject.complete();
+  }
 
+  loadInitialData(initialData: DynamicFlatNode[]) {
+    this.dataSubject.next(initialData);
+  }
+
+  loadChildren(node: DynamicFlatNode) {
+  }
+}
 
 @Component({
   selector: 'app-side-panel',
@@ -88,14 +57,13 @@ export class SidePanelComponent {
   dataSource: DynamicDataSource;
 
   constructor(database: DatabaseManagerService) {
-    this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
-    this.dataSource = new DynamicDataSource(this.treeControl);
+    this.treeControl = new FlatTreeControl<DynamicFlatNode>(node => node.level, node => node.isExpandable);
+    this.dataSource = new DynamicDataSource(database);
+
+    this.treeControl.expansionModel.changed.subscribe(change => {
+      console.log('change', change);
+    });
   }
 
-
-  getLevel = (node: DynamicFlatNode) => node.level;
-
-  isExpandable = (node: DynamicFlatNode) => node.expandable;
-
-  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
+  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.isExpandable;
 }
