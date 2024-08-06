57ee004990efa02a42275126164f6c0075f436e9
dplocki <dawid.plocki@gmail.com>
2024-08-06 22:41:55 +0200

    feat: remove test event source code

--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -2,8 +2,6 @@ import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MetaData, NgEventBus } from 'ng-event-bus';
-import { IEventDataUserSelectTableOrColumn } from '../utils/events.interfaces';
 
 @Component({
   selector: 'app-query',
@@ -19,16 +17,9 @@ import { IEventDataUserSelectTableOrColumn } from '../utils/events.interfaces';
 })
 export class QueryComponent {
   @Input()
-  public query: string = '';
+  public query: string|undefined = '';
 
-  constructor(
-    private eventBus: NgEventBus) {
-    this.eventBus.on('user:select:columnname').subscribe({
-      next: (event: MetaData<unknown>): void => {
-        const data = event as MetaData<IEventDataUserSelectTableOrColumn>;
-        console.log('event:', data);
-      }
-    });
+  constructor() {
   }
 
 }
