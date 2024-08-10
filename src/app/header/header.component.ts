602972d60c05a01ff4805226ef10dbdfdbe1e1d8
dplocki <dawid.plocki@gmail.com>
2024-08-10 13:22:55 +0200

    feat(header component): add draft for menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatMenuModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
