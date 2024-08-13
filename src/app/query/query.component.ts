c41c48c65b61837f9f8e3f6291b03a1bdc968467
dplocki <dawid.plocki@gmail.com>
2024-08-13 23:03:02 +0200

    feat(query component): add missing reference to material button module

--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
@@ -8,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
   standalone: true,
   imports: [
     FormsModule,
+    MatButtonModule,
     MatFormFieldModule,
     MatFormFieldModule,
     MatInputModule,
