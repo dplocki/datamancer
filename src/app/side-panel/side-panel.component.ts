5ea6490491ea45866316f878a0b1cb0ec5cadb3a
dplocki <dawid.plocki@gmail.com>
2024-08-17 10:38:42 +0200

    feat(side-panel component): add button for import data

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { TablesTreeComponent } from '../tables-tree/tables-tree.component';
+import { NgEventBus } from 'ng-event-bus';
 
 @Component({
   selector: 'app-side-panel',
@@ -15,4 +16,11 @@ import { TablesTreeComponent } from '../tables-tree/tables-tree.component';
   styleUrl: './side-panel.component.scss'
 })
 export class SidePanelComponent {
+
+  constructor(private eventBus: NgEventBus) {
+  }
+
+  public onImportClick() {
+    this.eventBus.cast('user:select:importdata')
+  }
 }
