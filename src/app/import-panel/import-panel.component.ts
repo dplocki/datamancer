3efe5445d8e030b6a931b665d4492b4c860c80e5
dplocki <dawid.plocki@gmail.com>
2024-09-06 22:05:26 +0200

    feat(import-panel): implement base parsing of json data

--- src/app/import-panel/import-panel.component.ts
+++ src/app/import-panel/import-panel.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-import-panel',
   standalone: true,
   imports: [
     FormsModule,
+    MatButtonModule,
     MatButtonToggleModule,
   ],
   templateUrl: './import-panel.component.html',
@@ -15,5 +17,11 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 export class ImportPanelComponent {
 
   public fontStyle: string = 'JSON';
+  public rawText: string = '';
 
+  public parseData(): void {
+    const data = JSON.parse(this.rawText);
+
+    console.log(data);
+  }
 }
