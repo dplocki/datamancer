0f30a846b6ed56225509ce4f58882464f4b5081a
dplocki <dawid.plocki@gmail.com>
2024-09-06 21:37:58 +0200

    feat(import-panel): add based draft of template

--- src/app/import-panel/import-panel.component.ts
+++ src/app/import-panel/import-panel.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-import-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './import-panel.component.html',
   styleUrl: './import-panel.component.scss'
 })
 export class ImportPanelComponent {
+
+  public fontStyle: string = 'JSON';
 
 }
