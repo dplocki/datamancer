621c123ba0d84b27d470535cc02f3b55349c8919
dplocki <dawid.plocki@gmail.com>
2024-09-07 12:53:59 +0200

    feat(table-view component): remove the downloading data from table-view

--- src/app/table-view/table-view.component.ts
+++ src/app/table-view/table-view.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { DatabaseManagerService } from '../services/database-manager.service';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 
 export interface PeriodicElement {
@@ -25,9 +24,7 @@ export interface PeriodicElement {
 export class TableViewComponent implements OnChanges, AfterViewInit {
 
   @Input()
-  public query!: string;
-
-  public error: string | null = null;
+  public data: any[] | null = null;
 
   public displayedColumns!: string[];
   public dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
@@ -35,7 +32,7 @@ export class TableViewComponent implements OnChanges, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator = <MatPaginator>{};
   @ViewChild(MatSort) sort: MatSort = <MatSort>{};
 
-  constructor(private databaseManager: DatabaseManagerService) {
+  constructor() {
   }
 
   public isTableEmpty(): boolean {
@@ -48,33 +45,19 @@ export class TableViewComponent implements OnChanges, AfterViewInit {
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
-    if (!changes['query']) {
+    if (!changes['data']) {
       return;
     }
 
-    try {
-      const query = changes['query'].currentValue;
-      if (query === '') {
-        this.error = null;
-        this.dataSource.data = [];
-        this.displayedColumns = [];
-        return;
-      }
-
-      const data = this.databaseManager.runQuery(query);
-      if (!data) {
-        this.dataSource.data = [];
-        this.error = null;
-        return;
-      }
-
-      this.displayedColumns = data.length > 0 ? Object.keys(data[0]) : [];
-      this.dataSource.data = data;
-      this.error = null;
-    } catch (error) {
-      this.error = (error as Error).message;
-      console.log(error);
+    const data = changes['data'].currentValue;
+    if (!data) {
+      this.dataSource.data = [];
+      this.displayedColumns = [];
+      return;
     }
+
+    this.displayedColumns = data.length > 0 ? Object.keys(data[0]) : [];
+    this.dataSource.data = data;
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator = this.paginator;
