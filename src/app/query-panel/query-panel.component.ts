b7e351ef296164fd5ab6b4427962f76640de28c4
dplocki <dawid.plocki@gmail.com>
2024-09-07 13:05:39 +0200

    feat(query-panel component): implement the query executing

--- src/app/query-panel/query-panel.component.ts
+++ src/app/query-panel/query-panel.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TableViewComponent } from '../table-view/table-view.component';
 import { QueryComponent } from '../query/query.component';
+import { DatabaseManagerService } from '../services/database-manager.service';
 
 @Component({
   selector: 'app-query-panel',
@@ -12,11 +13,25 @@ import { QueryComponent } from '../query/query.component';
   templateUrl: './query-panel.component.html',
   styleUrl: './query-panel.component.scss'
 })
-export class QueryPanelComponent {
+export class QueryPanelComponent implements OnInit {
   @Input()
-  public query!: string;
+  public query: string = '';
+  public data: any[] = [];
+
+  constructor(
+    private databaseManager: DatabaseManagerService) {
+  }
+
+  public ngOnInit(): void {
+    if (!this.query) {
+      return;
+    }
+
+    this.executeQuery(this.query);
+  }
 
   public executeQuery(query: string): void {
     this.query = query;
+    this.data = this.databaseManager.runQuery(query);
   }
 }
