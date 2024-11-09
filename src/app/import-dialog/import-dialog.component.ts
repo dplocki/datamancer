fe7b68614583322df3037c6cbc72a33826d99aab
dplocki <dawid.plocki@gmail.com>
2024-11-09 15:03:13 +0100

    feat(import-dialog component): implement general validation

--- src/app/import-dialog/import-dialog.component.ts
+++ src/app/import-dialog/import-dialog.component.ts
@@ -37,7 +37,7 @@ interface IValidationMessages {
 })
 export class ImportDialogComponent {
   readonly tableName = new FormControl('', [Validators.required]);
-  readonly dataTyp = new FormControl('', [Validators.required]);
+  readonly dataType = new FormControl('', [Validators.required]);
 
   public selectedFile: File | null = null;
   public uploadProgress = 0;
@@ -53,11 +53,9 @@ export class ImportDialogComponent {
     public dialogRef: MatDialogRef<ImportDialogComponent>,
   ) {
     this.dialogRef.disableClose = true;
-    this.dialogRef.beforeClosed().subscribe((result) => {
-      if (this.validation.dataType != null) {
-        result.preventDefault();
-      }
-    });
+
+    this.tableName.valueChanges.subscribe(this.onTableNameChange.bind(this));
+    this.dataType.valueChanges.subscribe(this.onDataTypeChange.bind(this));
   }
 
   public onFileSelected(event: Event): void {
@@ -74,14 +72,15 @@ export class ImportDialogComponent {
     );
 
     if (file.name.endsWith('.csv')) {
-      this.dataTyp.setValue('csv');
+      this.dataType.setValue('csv');
     } else if (file.name.endsWith('.json')) {
-      this.dataTyp.setValue('json');
+      this.dataType.setValue('json');
     } else {
-      this.dataTyp.setValue('');
+      this.dataType.setValue('');
     }
 
-    this.dataTyp.markAllAsTouched();
+    this.dataType.markAllAsTouched();
+    this.tableName.markAllAsTouched();
 
     if (file) {
       this.selectedFile = file;
@@ -94,10 +93,6 @@ export class ImportDialogComponent {
       return;
     }
 
-    if (this.dataTyp.hasError('required')) {
-      this.validation.dataType = 'Select the import method';
-      return;
-    }
 
     this.uploading = true;
 
@@ -121,7 +116,7 @@ export class ImportDialogComponent {
     this.dialogRef.close(true);
   }
 
-  public resetForm(event: Event): void {
+  public onFormReset(event: Event): void {
     this.selectedFile = null;
     this.uploadProgress = 0;
     this.uploading = false;
@@ -132,5 +127,21 @@ export class ImportDialogComponent {
     };
 
     event.stopPropagation();
+  }
+
+  public onTableNameChange() {
+    if (this.tableName.hasError('required')) {
+      this.validation.tableName = 'Provide the table name';
+    } else {
+      this.validation.tableName = null;
+    }
+  }
+
+  public onDataTypeChange() {
+    if (this.dataType.hasError('required')) {
+      this.validation.dataType = 'Select the import method';
+    } else {
+      this.validation.dataType = null;
+    }
   }
 }
