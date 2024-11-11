d91d6e17902e1cf30ad11c95eb9bc70d999095be
dplocki <dawid.plocki@gmail.com>
2024-11-11 13:13:26 +0100

    tests(error-display component): add unit tests for the error-display component

--- src/app/error-display/error-display.component.spec.ts
+++ src/app/error-display/error-display.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ErrorDisplayComponent } from './error-display.component';
 
 describe('ErrorDisplayComponent', () => {
@@ -8,9 +7,10 @@ describe('ErrorDisplayComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ErrorDisplayComponent]
-    })
-    .compileComponents();
+      imports: [
+        ErrorDisplayComponent,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorDisplayComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,31 @@ describe('ErrorDisplayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not display message when message input is undefined', () => {
+    component.message = undefined;
+    fixture.detectChanges();
+
+    const preElement = fixture.nativeElement.querySelector('pre');
+    expect(preElement).toBeNull();
+  });
+
+  it('should not display message when message input is null', () => {
+    component.message = null;
+    fixture.detectChanges();
+
+    const preElement = fixture.nativeElement.querySelector('pre');
+    expect(preElement).toBeNull();
+  });
+
+  it('should display message when message input is provided', () => {
+    const testMessage = 'Test error message';
+    component.message = testMessage;
+    fixture.detectChanges();
+
+    const preElement = fixture.nativeElement.querySelector('pre');
+    expect(preElement).toBeTruthy();
+    expect(preElement.textContent).toContain(testMessage);
   });
 });
